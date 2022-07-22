39bbeca3d6f595e311e103060ddafcb183773fec
Sunita Gurau <sunita.gurau@introcept.co>
2022-07-22 16:51:34 +0545

    password component design

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -1,18 +1,30 @@
 <script setup>
-import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/inertia-vue3';
+import BreezeApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/inertia-vue3";
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <div
+        id="login-background"
+        class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-no-repeat bg-cover"
+    >
         <div>
             <Link href="/">
-                <BreezeApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <BreezeApplicationLogo
+                    class="w-20 h-20 fill-current text-gray-500"
+                />
             </Link>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div
+            class="w-full sm:max-w-[400px] mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+        >
             <slot />
         </div>
     </div>
 </template>
+<style>
+#login-background {
+    background-image: url(../../images/loginbackground.png);
+}
+</style>
