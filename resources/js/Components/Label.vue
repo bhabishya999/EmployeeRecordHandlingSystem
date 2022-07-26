567a362bcccb1eda785f3c1cf114cdbe63a82f2c
Sunita Gurau <sunita.gurau@introcept.co>
2022-07-26 16:20:28 +0545

    wip

--- resources/js/Components/Label.vue
+++ resources/js/Components/Label.vue
@@ -1,9 +1,9 @@
 <script setup>
-defineProps(['value']);
+defineProps(["value"]);
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-700">
+    <label class="block font-medium text-sm text-gray-700 pb-2.5">
         <span v-if="value">{{ value }}</span>
         <span v-else><slot /></span>
     </label>
