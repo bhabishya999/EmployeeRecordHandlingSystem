77e4ae62927e010c1bb6563d935019000d8f688d
prabin khanal <prabina.khanal@introcept.co>
2022-07-26 09:56:36 +0545

    deleted unused and commented part

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -1,15 +1,11 @@
 <script setup>
-// import BreezeApplicationLogo from '@/Components/ApplicationLogo.vue';
+
 import { Link } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
     <div id="background" class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-origin-content  bg-norepeat bg-center bg-fixed">
-        <!-- <div>
-            <Link href="/">
-                <BreezeApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
-        </div> -->
+        
 
         <div class="w-full sm:max-w-[370px]  mt-6 px-6 py-4 bg-white shadow overflow-hidden ">
             <slot />
