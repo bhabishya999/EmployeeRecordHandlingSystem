f911ee7a682446c92b9fd13fa02086421fd887a7
Sunita Gurau <sunita.gurau@introcept.co>
2022-07-28 18:45:23 +0545

    design of Previous password error

---
+++ resources/js/Pages/Auth/MatchPsError.vue
@@ -0,0 +1,23 @@
+<template>
+    <GuestLayout>
+        <div id="password-background">
+            <MatchPsError></MatchPsError>
+        </div>
+    </GuestLayout>
+</template>
+
+<script>
+import PreviousPsError from "@/Components/PreviousPsError.vue";
+export default {
+    name: {
+        MatchPsError,
+    },
+    components: {
+        PreviousPsError,
+    },
+};
+</script>
+<style>
+#password-background {
+}
+</style>
