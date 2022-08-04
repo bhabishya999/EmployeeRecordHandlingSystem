8bdd92ef467234c14694136c16b6eff96676420b
Bhabishya Chaudhary <bhabishya.chaudhary@introcept.co>
2022-08-04 09:18:45 +0545

    Updated password PasswordResetMail.php

--- app/Mail/PasswordResetMail.php
+++ app/Mail/PasswordResetMail.php
@@ -3,33 +3,15 @@
 namespace App\Mail;
 
 use App\Models\User;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
 
 class PasswordResetMail extends Mailable
 {
-    use Queueable, SerializesModels;
-
-     /**
-     * The order instance.
-     *
-     */
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
     public function __construct(public User|Model $user,public string $token)
     {
     
     }
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
+
     public function build()
     {
         return $this->view('password_reset_email')
