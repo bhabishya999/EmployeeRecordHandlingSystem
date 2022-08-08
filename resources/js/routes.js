3603638993129e062779a44d5bd583d10d906229
Sunita Gurau <sunita.gurau@introcept.co>
2022-08-08 17:17:28 +0545

    setup protected routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,7 @@
 import Login from '@/Pages/Auth/Login.vue';
 import ForgotPassword from '@/Pages/Auth/ForgotPassword.vue';
 import AddEmployeeDetails from "@/Pages/Auth/AddEmployeeDetails.vue"
+import Employees from "@/Pages/Employees/Employees.vue"
 import { createWebHistory, createRouter } from 'vue-router'
 const routes = [
     {
@@ -24,6 +25,13 @@ const routes = [
         component: AddEmployeeDetails
 
     },
+    {
+        name: 'Employees',
+        path: '/employees',
+        requiresAuth: true,
+        component: Employees
+
+    },
 
 ];
 
@@ -31,5 +39,25 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 })
+router.beforeEach((to, from, next) => {
+    const token = localStorage.getItem('token')
+    const authenticated = !!token
+    const shouldbeAuthenticated = to.matched.some(
+        record => record.meta.requiresAuth
+    );
+    if (shouldbeAuthenticated) {
+
+        if (!authenticated) {
+            next({
+                path: "/login",
+                query: { redirect: to.fullPath }
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
 export default router
 
