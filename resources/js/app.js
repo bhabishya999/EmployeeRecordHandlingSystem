dda7ee9310ccf361c74d4bdfae69b7f4e4984175
lamasagar <sagar.lama@globaly.io>
2022-08-09 10:22:33 +0545

    fix: Fix vue routing issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,7 @@
 import "../css/app.css";
+import "./axios";
 import "./bootstrap";
-import "./axios"
 
 import router from "@/routes";
 import { createApp } from "vue";
-
 createApp({}).use(router).mount("#app");
