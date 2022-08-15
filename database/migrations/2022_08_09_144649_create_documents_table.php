53da2b0da3497ae1881e5a0addf24564df99f0e9
Bhabishya Chaudhary <bhabishya.chaudhary@introcept.co>
2022-08-15 14:01:33 +0545

    Updated documents table

--- database/migrations/2022_08_09_144649_create_documents_table.php
+++ database/migrations/2022_08_09_144649_create_documents_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('documents', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('type');
             $table->string('path');
             $table->timestamps();
