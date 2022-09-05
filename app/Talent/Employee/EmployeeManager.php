f86c8470016516df552cb2e4369355601e04a212
Bhabishya Chaudhary <bhabishya.chaudhary@introcept.co>
2022-09-05 12:22:12 +0545

    Created Filter API

--- app/Talent/Employee/EmployeeManager.php
+++ app/Talent/Employee/EmployeeManager.php
@@ -15,13 +15,17 @@ class EmployeeManager
         $employee= $this->employee->create($employeeArray);
         return $employee;
     }
-    public function employeeList(?int $perPage=10, ?string $search=null):LengthAwarePaginator
+    public function employeeList(?int $perPage=10, ?string $search=null, ?string $filter=null):LengthAwarePaginator
     {
         return $this->employee->with('employment:employee_id,current_position,work_schedule,team')
         ->when(!empty($search),function($query) use ($search)
         {
         $query->where('first_name','LIKE','%'.$search.'%')->orWhere('last_name','LIKE','%'.$search.'%')
         ->orWhere('email','LIKE','%'.$search.'%');
+        })
+        ->when(!empty($filter),function($query) use ($filter)
+        {
+        $query->where('status',$filter)->orWhereRelation('employment','team',$filter);
         })
         ->select(['id','first_name','last_name','email','status','avatar','contact_number'])
         ->paginate($perPage);
