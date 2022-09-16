abdeb10e90ed95cf27965afe1f658bfeda473e09
Trishana9 <trishana.rai@introcept.co>
2022-09-17 01:33:19 +0545

    displaying data in the form

--- resources/js/Components/EditEducationalDetail.vue
+++ resources/js/Components/EditEducationalDetail.vue
@@ -1,160 +1,58 @@
-<script setup>
-import Details from "@/Layouts/Details.vue";
-</script>
 <template>
-  <div>
-    <Form
-      @submit="onSubmit"
-      :validation-schema="schema"
-      :initial-values="initialData"
-    >
+  <div class="px-2 py-5">
+    <Form @submit="onSubmit">
       <div class="flex flex-col w-2/3 mr-10">
-        <FieldArray name="users" v-slot="{ fields, push, remove }">
-          <fieldset
-            class="InputGroup mb-2.5"
-            v-for="(field, index) in fields"
-            :key="field.key"
-          >
-            <div class="flex justify-between">
-              <legend
-                class="
-                  block
-                  font-bold
-                  text-sm text-indigo-700
-                  pb-2.5
-                  leading-normal
-                  placeholder:text-slate-500
-                  placeholder:text-base
-                  placeholder:font-normal
-                "
+        <div v-for="(edutionalList, index) in edutionalList" v-bind:key="index">
+          <div class="flex justify-between">
+            <p
+              class="
+                block
+                font-bold
+                text-sm text-indigo-700
+                pb-2.5
+                leading-normal
+                placeholder:text-slate-500
+                placeholder:text-base
+                placeholder:font-normal
+              "
+            >
+              Education {{ index + 1 }}
+            </p>
+            <button
+              @click="deleteEducation(index)"
+              v-show="index > 0"
+              type="button"
+              class="
+                py-[7px]
+                px-2.5
+                text-base
+                font-bold
+                flex
+                items-center
+                justify-center
+                text-danger
+              "
+            >
+              <svg
+                class="mr-2"
+                width="14"
+                height="15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                Education{{ index + 1 }}
-              </legend>
-              <button
-                @click="remove(index)"
-                v-show="index > 0"
-                type="button"
-                class="
-                  py-[7px]
-                  px-2.5
-                  text-base
-                  font-bold
-                  flex
-                  items-center
-                  justify-center
-                  text-danger
-                "
-              >
-                <svg
-                  class="mr-2"
-                  width="14"
-                  height="15"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M4.25 2.5c0-.966.784-1.75 1.75-1.75h2c.966 0 1.75.784 1.75 1.75v.75H13a.75.75 0 0 1 0 1.5h-.787l-.369 7.387a2.75 2.75 0 0 1-2.746 2.613H4.902a2.75 2.75 0 0 1-2.746-2.613l-.37-7.387H1a.75.75 0 0 1 0-1.5h3.25V2.5Zm1.5.75h2.5V2.5A.25.25 0 0 0 8 2.25H6a.25.25 0 0 0-.25.25v.75ZM6 6.463a.75.75 0 1 0-1.5.074l.25 5a.75.75 0 0 0 1.5-.075l-.25-5Zm3.5.074A.75.75 0 1 0 8 6.463l-.25 5a.75.75 0 0 0 1.5.075l.25-5Z"
-                    fill="#C53030"
-                  />
-                </svg>
-                Remove
-              </button>
-            </div>
-            <div class="flex flex-row justify-between mb-2.5">
-              <div class="w-full mr-10">
-                <div class="flex flex-col">
-                  <label
-                    :for="`education_${index}`"
-                    class="
-                      block
-                      font-normal
-                      text-sm text-gray-900
-                      pb-2.5
-                      leading-normal
-                      placeholder:text-slate-500
-                      placeholder:text-base
-                      placeholder:font-normal
-                    "
-                    >Education</label
-                  >
-                  <Field
-                    :id="`education_${index}`"
-                    :name="`users[${index}].education_level`"
-                    type="text"
-                    placeholder="Higher Secondary Level"
-                    class="
-                      bg-white
-                      pb-3.5
-                      pt-4
-                      pl-4
-                      w-full
-                      rounded-md
-                      text-sm
-                      leading-normal
-                      text-slate-500
-                      font-normal font-sans
-                      border-solid border-2
-                      outline-indigo-700 outline-2
-                      border-slate-200
-                      active:border-indigo-700
-                    "
-                  />
-                  <ErrorMessage
-                    :name="`users[${index}].education_level`"
-                    class="text-red-600"
-                  />
-                </div>
-              </div>
-              <div class="w-full">
-                <div class="flex flex-col">
-                  <label
-                    :for="`year_${index}`"
-                    class="
-                      block
-                      font-normal
-                      text-sm text-gray-900
-                      pb-2.5
-                      leading-normal
-                      placeholder:text-slate-500
-                      placeholder:text-base
-                      placeholder:font-normal
-                    "
-                    >Passed Year(A.D)</label
-                  >
-                  <Field
-                    type="number"
-                    :id="`year_${index}`"
-                    :name="`users[${index}].passed_year`"
-                    placeholder="2017"
-                    class="
-                      bg-white
-                      pb-3.5
-                      pt-4
-                      pl-4
-                      w-full
-                      rounded-md
-                      text-sm
-                      leading-normal
-                      text-slate-500
-                      font-normal font-sans
-                      border-solid border-2
-                      outline-indigo-700 outline-2
-                      border-slate-200
-                      active:border-indigo-700
-                    "
-                  />
-                  <ErrorMessage
-                    :name="`users[${index}].passed_year`"
-                    class="text-red-600"
-                  />
-                </div>
-              </div>
-            </div>
-            <div class="flex flex-col">
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M4.25 2.5c0-.966.784-1.75 1.75-1.75h2c.966 0 1.75.784 1.75 1.75v.75H13a.75.75 0 0 1 0 1.5h-.787l-.369 7.387a2.75 2.75 0 0 1-2.746 2.613H4.902a2.75 2.75 0 0 1-2.746-2.613l-.37-7.387H1a.75.75 0 0 1 0-1.5h3.25V2.5Zm1.5.75h2.5V2.5A.25.25 0 0 0 8 2.25H6a.25.25 0 0 0-.25.25v.75ZM6 6.463a.75.75 0 1 0-1.5.074l.25 5a.75.75 0 0 0 1.5-.075l-.25-5Zm3.5.074A.75.75 0 1 0 8 6.463l-.25 5a.75.75 0 0 0 1.5.075l.25-5Z"
+                  fill="#C53030"
+                />
+              </svg>
+              Remove
+            </button>
+          </div>
+          <div class="flex flex-row justify-between mb-2.5">
+            <div class="w-full mr-10">
               <label
-                :for="`institution_${index}`"
                 class="
                   block
                   font-normal
@@ -165,13 +63,11 @@ import Details from "@/Layouts/Details.vue";
                   placeholder:text-base
                   placeholder:font-normal
                 "
-                >Institution</label
+                for="education"
+                >Education</label
               >
-              <Field
-                :id="`institution_${index}`"
-                :name="`users[${index}].institution`"
+              <input
                 type="text"
-                placeholder="United Academy"
                 class="
                   bg-white
                   pb-3.5
@@ -188,59 +84,127 @@ import Details from "@/Layouts/Details.vue";
                   border-slate-200
                   active:border-indigo-700
                 "
-              />
-              <ErrorMessage
-                :name="`users[${index}].institution`"
-                class="text-red-600"
+                v-model="edutionalList.education_level"
+                placeholder="Higher Secondary Level"
+                name="education_level"
               />
             </div>
-          </fieldset>
-
-          <div class="border-b-2 border-slate-100 w-full my-5"></div>
-          <button
-            @click="
-              push({
-                education_level: '',
-                passed_year: '',
-                institution: '',
-              })
-            "
-            type="button"
-            class="
-              w-32
-              mr-2.5
-              py-[7px]
-              px-2.5
-              bg-slate-100
-              rounded-md
-              shadow
-              text-base
-              font-bold
-              flex
-              items-center
-              text-primary
-            "
-          >
-            <svg
-              class="mr-2"
-              width="11"
-              height="12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5.5 1.5v9M1 6h9"
-                stroke="#4C51BF"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+            <div class="w-full">
+              <label
+                class="
+                  block
+                  font-normal
+                  text-sm text-gray-900
+                  pb-2.5
+                  leading-normal
+                  placeholder:text-slate-500
+                  placeholder:text-base
+                  placeholder:font-normal
+                "
+                for="passedYear"
+                >Passed Year(A.D)</label
+              >
+              <input
+                class="
+                  bg-white
+                  pb-3.5
+                  pt-4
+                  pl-4
+                  w-full
+                  rounded-md
+                  text-sm
+                  leading-normal
+                  text-slate-500
+                  font-normal font-sans
+                  border-solid border-2
+                  outline-indigo-700 outline-2
+                  border-slate-200
+                  active:border-indigo-700
+                "
+                type="text"
+                v-model="edutionalList.passed_year"
+                name="passed_year"
+                placeholder="2017"
               />
-            </svg>
-            Add more
-          </button>
-        </FieldArray>
+            </div>
+          </div>
+          <div class="w-full">
+            <label
+              class="
+                block
+                font-normal
+                text-sm text-gray-900
+                pb-2.5
+                leading-normal
+                placeholder:text-slate-500
+                placeholder:text-base
+                placeholder:font-normal
+              "
+              for="institution"
+              >Institution</label
+            >
+            <input
+              class="
+                bg-white
+                pb-3.5
+                pt-4
+                pl-4
+                w-full
+                rounded-md
+                text-sm
+                leading-normal
+                text-slate-500
+                font-normal font-sans
+                border-solid border-2
+                outline-indigo-700 outline-2
+                border-slate-200
+                active:border-indigo-700
+              "
+              type="text"
+              v-model="edutionalList.institution"
+              name="institution"
+              placeholder="United Academy"
+            />
+          </div>
+          <div class="border-b-2 border-slate-100 w-full my-5"></div>
+        </div>
+
+        <button
+          @click="addEducation"
+          type="button"
+          class="
+            w-32
+            mr-2.5
+            py-[7px]
+            px-2.5
+            bg-slate-100
+            rounded-md
+            shadow
+            text-base
+            font-bold
+            flex
+            items-center
+            text-primary
+          "
+        >
+          <svg
+            class="mr-2"
+            width="11"
+            height="12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5.5 1.5v9M1 6h9"
+              stroke="#4C51BF"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          Add more
+        </button>
       </div>
-      <div class="border-b-2 border-slate-100 w-full"></div>
       <div>
         <div class="flex flex-row-reverse items-center px-9 py-2.5">
           <Button
@@ -266,7 +230,7 @@ import Details from "@/Layouts/Details.vue";
               font-sans
             "
           >
-            Save Changes
+            Save and Continue
           </Button>
           <button
             type="button"
@@ -288,6 +252,7 @@ import Details from "@/Layouts/Details.vue";
       <div
         v-if="togglePopUp"
         class="
+          z-50
           fixed
           inset-0
           w-full
@@ -326,87 +291,60 @@ import Details from "@/Layouts/Details.vue";
     </Form>
   </div>
 </template>
-    <script>
-import NavBar from "@/Components/NavBar.vue";
-import { ErrorMessage, Field, FieldArray, Form } from "vee-validate";
-import TogglePopUp from "@/Components/TogglePopUp.vue";
+
+<script>
 import Button from "@/Components/Button.vue";
+import { Form } from "vee-validate";
+import CustomInput from "@/Components/CustomInput.vue";
+import TogglePopUp from "@/Components/TogglePopUp.vue";
 import * as Yup from "yup";
 import { ref } from "vue";
 export default {
-  name: "EditEducationalDetails",
-  Components: {
-    NavBar,
-    Button,
-    Field,
+  name: "EditEducationalDetail",
+  components: {
     Form,
-    ErrorMessage,
-    FieldArray,
-    Yup,
+    Button,
+    CustomInput,
     TogglePopUp,
+    Yup,
   },
-
   data() {
-    const initialData = {
-      users: [
-        {
-          education_level: "",
-          passed_year: "",
-          institution: "",
-          employee_id: "",
-        },
-      ],
-    };
-    const schema = Yup.object().shape({
-      users: Yup.array()
-        .of(
-          Yup.object().shape({
-            education_level: Yup.string().required().label("Education"),
-            passed_year: Yup.string().max(4).required().label("Year"),
-            institution: Yup.string().required().label("Institution"),
-          })
-        )
-        .strict(),
-    });
     const togglePopUp = ref(false);
     return {
-      initialData,
-      schema,
+      edutionalList: [],
       togglePopUp,
-      edutionalDetailList: [],
       isLoading: false,
     };
   },
-
   methods: {
-    onSubmit(values) {
-      this.togglePopUp = true;
+    addEducation() {
+      this.edutionalList.push({
+        education_level: "",
+        passed_year: "",
+        institution: "",
+      });
+    },
+    deleteEducation(index) {
+      this.edutionalList.splice(index, 1);
+    },
+    onSubmit() {
+      const educational_details = this.edutionalList.map((edu) => {
+        return edu;
+      });
     },
   },
   created() {
     axios
       .get(`employees/educations/11`)
       .then(({ data }) => {
-        console.log(this.initialData.users);
         console.log(data.data);
-        const edutionalDetail = data.data.educational_details;
-        console.log(edutionalDetail);
-        this.edutionalDetailList = edutionalDetail.map(
-          ({ education_level, passed_year, institution }) => ({
-            education_level: education_level,
-            passed_year: passed_year,
-            institution: institution,
-          })
-        );
-        let i;
-        for (i = 0; i < this.edutionalDetailList.length; i++) {
-          this.initialData["users"].push(this.edutionalDetailList[i]);
-        }
+        const edutionalDetailList = data.data.educational_details;
+        edutionalDetailList.forEach((edu) => {
+          this.edutionalList.push(edu);
+        });
       })
       .catch((error) => console.log(error));
   },
 };
 </script>
-    <style>
-</style>
-      
+  
