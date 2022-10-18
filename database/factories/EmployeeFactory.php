dc3fd25eb20eaee7eac34cc8c6471d570b34d81a
sushant paneru <sushant.paneru@introcept.co>
2022-10-18 12:38:14 +0545

    editing EmployeeFactory status

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -16,7 +16,7 @@ class EmployeeFactory extends Factory
     public function definition(): array
     {
         return [
-            'status' => rand(0,1),
+            'status' => $this->faker->randomElement(['Active', 'Alumni']),
             'first_name' => $this->faker->firstName(),
             'last_name' => $this->faker->lastName(),
             'contact_number' => $this->faker->phoneNumber(),
