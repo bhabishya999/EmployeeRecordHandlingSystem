7d58a1b755ae029d485c69c91a501e303a86590e
Trishana9 <trishana.rai@introcept.co>
2022-10-19 10:17:36 +0545

    added side navigation bar to all pages and fixed the alignments

--- resources/js/Layouts/Details.vue
+++ resources/js/Layouts/Details.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="bg-[#EDF2F7] pb-2">
-        <div
-            class="bg-white drop-shadow-[0_1px_2px_rgba(0,0,0,0.06)_0_10px_15px_rgba(0,0,0,0.1)] mx-40"
-        >
-            <slot />
-        </div>
+  <div class="pb-2">
+    <div
+      class="
+        bg-white
+        drop-shadow-[0_1px_2px_rgba(0,0,0,0.06)_0_10px_15px_rgba(0,0,0,0.1)]
+        px-10
+      "
+    >
+      <slot />
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: "Details",
-    components: {},
+  name: "Details",
+  components: {},
 };
 </script>
--- resources/js/Pages/Employees/Employees.vue
+++ resources/js/Pages/Employees/Employees.vue
@@ -1,173 +1,144 @@
 <template>
-  <div>
-    <NavBar />
-    <div class="h-10 w-full bg-light_blue">
-      <div
-        v-show="showSuccess"
-        class="w-full h-10 flex justify-center items-center bg-dark_green"
-      >
-        <svg
-          width="20"
-          height="12"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M18.51 1 6.472 10.625 1 6.25"
-            stroke="#fff"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        <p class="font-bold pl-2 text-lg text-white">
-          Employee has been sucessfully added.
-        </p>
-      </div>
-
-      <div
-        v-show="importSuccess"
-        class="w-full h-10 flex justify-center items-center bg-dark_green"
-      >
-        <svg
-          width="20"
-          height="12"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M18.51 1 6.472 10.625 1 6.25"
-            stroke="#fff"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        <p class="font-bold pl-2 text-lg text-white">
-          Data has been imported sucessfully.
-        </p>
-      </div>
+  <div class="flex">
+    <div class="w-[237px]">
+      <SideNavigationBar />
     </div>
+    <div class="w-full">
+      <NavBar>
+        <template v-slot:headerName :headerName="headerName"
+          >EMPLOYEES</template
+        >
+      </NavBar>
+      <div class="h-7">
+        <div
+          v-show="showSuccess"
+          class="w-full h-7 flex justify-center items-center bg-dark_green"
+        >
+          <svg
+            width="20"
+            height="12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M18.51 1 6.472 10.625 1 6.25"
+              stroke="#fff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <p class="font-bold pl-2 text-lg text-white">
+            Employee has been sucessfully added.
+          </p>
+        </div>
 
-    <div class="min-h-screen bg-light_blue px-40">
-      <div
-        class="
-          bg-white
-          w-full
-          drop-shadow-[0_1px_2px_rgba(0,0,0,0.06)_0_10px_15px_rgba(0,0,0,0.1)]
-        "
-      >
-        <EmployeeListHeader>
-          <template v-slot:total :total="total">{{ total }}</template>
-          <template v-slot:filter>
-            <select
-              name="status"
-              id="status"
-              :key="status"
-              @change="filterstatus($event)"
+        <div
+          v-show="importSuccess"
+          class="w-full h-7 flex justify-center items-center bg-dark_green"
+        >
+          <svg
+            width="20"
+            height="12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M18.51 1 6.472 10.625 1 6.25"
+              stroke="#fff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          <p class="font-bold pl-2 text-lg text-white">
+            Data has been imported sucessfully.
+          </p>
+        </div>
+      </div>
+
+      <div class="px-10">
+        <div
+          class="
+            bg-white
+            w-full
+            drop-shadow-[0_1px_2px_rgba(0,0,0,0.06)_0_10px_15px_rgba(0,0,0,0.1)]
+          "
+        >
+          <EmployeeListHeader>
+            <div
               class="
-                border-transparent
-                focus:border-transparent focus:ring-0
-                outline-0
-                scroll-smooth
-                border
-                drop-shadow
-                justify-between
+                h-[37px]
+                w-[220px]
+                pl-2
+                border border-primary
+                shadow-sm
                 rounded-md
-                text-primary
-                pr-16
+                flex
+                justify-center
+                items-center
+                text-center
+                bg-white
+                font-sans
+                not-italic
                 font-bold
+                text-base
               "
             >
-              &nbsp;&nbsp;
-
-              <option
-                v-for="status in status"
-                v-bind:key="status.status"
-                v-bind:value="status.status"
+              <button type="submit">
+                <svg
+                  width="13"
+                  height="14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5.823 11.667c2.663 0 4.821-2.388 4.821-5.334C10.644 3.388 8.486 1 5.823 1S1 3.388 1 6.333c0 2.946 2.159 5.334 4.822 5.334ZM11.85 13l-2.622-2.9"
+                    stroke="#A0AEC0"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+              <input
+                v-model="search"
+                type="text"
+                placeholder="Search by name"
+                v-on:keyup.enter="filterSearch"
                 class="
-                  border-transparent
-                  hover:bg-primary_blue
-                  focus:border-transparent focus:ring-0
                   border-0
-                  outline-0
-                  scroll-smooth
-                  text-light_iris
-                  shadow-md
-                  rounded-sm
+                  h-[20px]
+                  w-[160px]
+                  placeholder:text-light_cyanblue
+                  pl-2
+                  text-black
+                  rounded-md
+                  border-transparent
+                  focus:border-transparent focus:ring-0
                 "
-              >
-                {{ status.status }}
-              </option>
-            </select>
-          </template>
+              />
+            </div>
+          </EmployeeListHeader>
 
-          <div
-            class="
-              h-[37px]
-              w-[220px]
-              pl-2
-              border border-primary
-              shadow-sm
-              rounded-md
-              flex
-              justify-center
-              items-center
-              text-center
-              bg-white
-              font-sans
-              not-italic
-              font-bold
-              text-base
-            "
+          <EmployeeList
+            v-for="list in employeeList"
+            :key="list.id"
+            :list="list"
           >
-            <button type="submit">
-              <svg
-                width="13"
-                height="14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5.823 11.667c2.663 0 4.821-2.388 4.821-5.334C10.644 3.388 8.486 1 5.823 1S1 3.388 1 6.333c0 2.946 2.159 5.334 4.822 5.334ZM11.85 13l-2.622-2.9"
-                  stroke="#A0AEC0"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </button>
-            <input
-              v-model="search"
-              type="text"
-              placeholder="Search by name"
-              v-on:keyup.enter="filterSearch"
-              class="
-                border-0
-                h-[20px]
-                w-[160px]
-                placeholder:text-light_cyanblue
-                pl-2
-                text-black
-                rounded-md
-                border-transparent
-                focus:border-transparent focus:ring-0
-              "
-            />
-          </div>
-        </EmployeeListHeader>
+          </EmployeeList>
+        </div>
 
-        <EmployeeList v-for="list in employeeList" :key="list.id" :list="list">
-        </EmployeeList>
+        <div class="flex justify-center">
+          <Pagination
+            v-on:pageClick="pageChange($event)"
+            v-on:pageDecrease="prev($event)"
+            :page_Number="pageNumber"
+            :total="total"
+          >
+          </Pagination>
+        </div>
       </div>
-
-      <Pagination
-        v-on:pageClick="pageChange($event)"
-        v-on:pageDecrease="prev($event)"
-        :page_Number="pageNumber"
-        :total="total"
-      >
-      </Pagination>
     </div>
   </div>
 </template>
@@ -177,7 +148,7 @@ import NavBar from "@/Components/NavBar.vue";
 import EmployeeList from "@/Components/EmployeeList.vue";
 import EmployeeListHeader from "@/Components/EmployeeListHeader.vue";
 import Pagination from "@/Components/Pagination.vue";
-
+import SideNavigationBar from "@/Components/SideNavigationBar.vue";
 export default {
   name: "Employees",
 
@@ -231,6 +202,7 @@ export default {
     EmployeeList,
     EmployeeListHeader,
     Pagination,
+    SideNavigationBar,
   },
 
   methods: {
