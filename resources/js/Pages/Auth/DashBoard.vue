1294a5b5479a84029cda4403668b1e510429ebc9
Trishana9 <trishana.rai@introcept.co>
2022-10-19 10:16:03 +0545

    created dashboard page

---
+++ resources/js/Pages/Auth/DashBoard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex">
+    <div class="w-[237px]">
+      <SideNavigationBar />
+    </div>
+    <div class="w-full">
+      <NavBar>
+        <template v-slot:headerName :headerName="headerName"
+          >DASHBOARD</template
+        >
+      </NavBar>
+    </div>
+  </div>
+</template>
+  <script>
+import Details from "@/Layouts/Details.vue";
+import NavBar from "@/Components/NavBar.vue";
+import SideNavigationBar from "@/Components/SideNavigationBar.vue";
+export default {
+  name: "DashBoard",
+  components: {
+    Details,
+    NavBar,
+    SideNavigationBar,
+  },
+};
+</script>
+
+  
